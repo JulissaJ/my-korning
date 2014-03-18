456b01adb75dcfc09de7d05b3aaf90263dd199d2
Gregory Sheppard <gregory.sheppard@live.com>
2014-03-18 18:09:43 -0400

    Added Products model and populated database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140318211943) do
+ActiveRecord::Schema.define(version: 20140318215359) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 20140318211943) do
     t.string   "first_name", null: false
     t.string   "last_name",  null: false
     t.string   "email",      null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "products", force: true do |t|
+    t.string   "name",       null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
